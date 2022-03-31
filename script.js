f0f3d6508cfc4c9ad8901924dd9cfa2ba77cf95f
NasDevFe <85785901+NasDevFe@users.noreply.github.com>
2022-03-31 23:29:06 +0530

    Second Commit - creating functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log(`connected`);
+//Password Generating functon//
+function genPassword(){
+var password =  document.getElementById("#password");
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength = 12;
+var password = "";
+   for(i=0; i<=passwordLength; i++){
+        var randomNumber = Math.floor(Math.random() * chars.length)
+            password = chars.substring(randomNumber, randomNumber+1) + password;
+   }
+    document.getElementById("password").value = password;
+}
+
+//Copy password function//
+    function copyPassword(){
+        var copyText = document.getElementById("password");
+        copyText.select();
+        document.execCommand('copy');  
+    }
+
+//End-of-code//
